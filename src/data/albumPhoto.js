6814c9b7560297e610df64f8410bd981291b9638
Toure <mohamedissiagat@gmail.com>
2026-07-13 02:20:29 +0000

    fix: noms fichiers album en minuscules

--- src/data/albumPhoto.js
+++ src/data/albumPhoto.js
@@ -1,36 +1,15 @@
-// Import des 15 photos depuis le sous-dossier album-photo
-import photoA from '../assets/album-photo/photoA.jpg'
-import photoB from '../assets/album-photo/photoB.jpg'
-import photoC from '../assets/album-photo/photoC.jpg'
-import photoD from '../assets/album-photo/photoD.jpg'
-import photoE from '../assets/album-photo/photoE.jpg'
-import photoF from '../assets/album-photo/photoF.jpeg'
-import photoG from '../assets/album-photo/photoG.jpg'
-import photoH from '../assets/album-photo/photoH.jpg'
-import photoI from '../assets/album-photo/photoI.jpeg'
-import photoJ from '../assets/album-photo/photoJ.jpeg'
-import photoK from '../assets/album-photo/photoK.jpeg'
-import photoL from '../assets/album-photo/photoL.jpeg'
-import photoM from '../assets/album-photo/photoM.png'
-import photoN from '../assets/album-photo/photoN.png'
-import photoO from '../assets/album-photo/photoO.jpeg'
-
-const albumPhoto = [
-  { id: 1, src: photoA },
-  { id: 2, src: photoB },
-  { id: 3, src: photoC },
-  { id: 4, src: photoD },
-  { id: 5, src: photoE },
-  { id: 6, src: photoF },
-  { id: 7, src: photoG },
-  { id: 8, src: photoH },
-  { id: 9, src: photoI },
-  { id: 10, src: photoJ },
-  { id: 11, src: photoK },
-  { id: 12, src: photoL },
-  { id: 13, src: photoM },
-  { id: 14, src: photoN },
-  { id: 15, src: photoO },
-]
-
-export default albumPhoto
+import photoA from '../assets/album-photo/photoa.jpg'
+import photoB from '../assets/album-photo/photob.jpg'
+import photoC from '../assets/album-photo/photoc.jpg'
+import photoD from '../assets/album-photo/photod.jpg'
+import photoE from '../assets/album-photo/photoe.jpg'
+import photoF from '../assets/album-photo/photof.jpeg'
+import photoG from '../assets/album-photo/photog.jpg'
+import photoH from '../assets/album-photo/photoh.jpg'
+import photoI from '../assets/album-photo/photoi.jpeg'
+import photoJ from '../assets/album-photo/photoj.jpeg'
+import photoK from '../assets/album-photo/photok.jpeg'
+import photoL from '../assets/album-photo/photol.jpeg'
+import photoM from '../assets/album-photo/photom.png'
+import photoN from '../assets/album-photo/photon.png'
+import photoO from '../assets/album-photo/photoo.jpeg'
