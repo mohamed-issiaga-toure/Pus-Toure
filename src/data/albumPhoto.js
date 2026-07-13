9c0540629fd7d2f3074b5f3356ad630454b05fef
Toure <mohamedissiagat@gmail.com>
2026-07-13 02:26:46 +0000

    fix: exportation de album

--- src/data/albumPhoto.js
+++ src/data/albumPhoto.js
@@ -13,3 +13,24 @@ import photoL from '../assets/album-photo/photol.jpeg'
 import photoM from '../assets/album-photo/photom.png'
 import photoN from '../assets/album-photo/photon.png'
 import photoO from '../assets/album-photo/photoo.jpeg'
+
+// Tableau des 15 photos
+const albumPhoto = [
+  { id: 1,  src: photoA },
+  { id: 2,  src: photoB },
+  { id: 3,  src: photoC },
+  { id: 4,  src: photoD },
+  { id: 5,  src: photoE },
+  { id: 6,  src: photoF },
+  { id: 7,  src: photoG },
+  { id: 8,  src: photoH },
+  { id: 9,  src: photoI },
+  { id: 10, src: photoJ },
+  { id: 11, src: photoK },
+  { id: 12, src: photoL },
+  { id: 13, src: photoM },
+  { id: 14, src: photoN },
+  { id: 15, src: photoO },
+]
+
+export default albumPhoto
