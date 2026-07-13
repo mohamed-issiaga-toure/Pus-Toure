8eb8a8e30b7bbbaaf56f54e85b586b72e3a85ea5
Toure <mohamedissiagat@gmail.com>
2026-07-13 02:33:11 +0000

    fix: modificatio des import dans l' album

--- src/data/albumPhoto.js
+++ src/data/albumPhoto.js
@@ -1,36 +1,36 @@
-import photoA from '../assets/album-photo/photoa.jpg'
-import photoB from '../assets/album-photo/photob.jpg'
-import photoC from '../assets/album-photo/photoc.jpg'
-import photoD from '../assets/album-photo/photod.jpg'
-import photoE from '../assets/album-photo/photoe.jpg'
-import photoF from '../assets/album-photo/photof.jpeg'
-import photoG from '../assets/album-photo/photog.jpg'
-import photoH from '../assets/album-photo/photoh.jpg'
-import photoI from '../assets/album-photo/photoi.jpeg'
-import photoJ from '../assets/album-photo/photoj.jpeg'
-import photoK from '../assets/album-photo/photok.jpeg'
-import photoL from '../assets/album-photo/photol.jpeg'
-import photoM from '../assets/album-photo/photom.png'
-import photoN from '../assets/album-photo/photon.png'
-import photoO from '../assets/album-photo/photoo.jpeg'
+import photoa from '../assets/album-photo/photoa.jpg'
+import photob from '../assets/album-photo/photob.jpg'
+import photoc from '../assets/album-photo/photoc.jpg'
+import photod from '../assets/album-photo/photod.jpg'
+import photoe from '../assets/album-photo/photoe.jpg'
+import photof from '../assets/album-photo/photof.jpeg'
+import photog from '../assets/album-photo/photog.jpg'
+import photoh from '../assets/album-photo/photoh.jpg'
+import photoi from '../assets/album-photo/photoi.jpeg'
+import photoj from '../assets/album-photo/photoj.jpeg'
+import photok from '../assets/album-photo/photok.jpeg'
+import photol from '../assets/album-photo/photol.jpeg'
+import photom from '../assets/album-photo/photom.png'
+import photon from '../assets/album-photo/photon.png'
+import photoo from '../assets/album-photo/photoo.jpeg'
 
 // Tableau des 15 photos
 const albumPhoto = [
-  { id: 1,  src: photoA },
-  { id: 2,  src: photoB },
-  { id: 3,  src: photoC },
-  { id: 4,  src: photoD },
-  { id: 5,  src: photoE },
-  { id: 6,  src: photoF },
-  { id: 7,  src: photoG },
-  { id: 8,  src: photoH },
-  { id: 9,  src: photoI },
-  { id: 10, src: photoJ },
-  { id: 11, src: photoK },
-  { id: 12, src: photoL },
-  { id: 13, src: photoM },
-  { id: 14, src: photoN },
-  { id: 15, src: photoO },
+  { id: 1,  src: photoa },
+  { id: 2,  src: photob },
+  { id: 3,  src: photoc },
+  { id: 4,  src: photod },
+  { id: 5,  src: photoe },
+  { id: 6,  src: photof },
+  { id: 7,  src: photog },
+  { id: 8,  src: photoh },
+  { id: 9,  src: photoi },
+  { id: 10, src: photoj },
+  { id: 11, src: photok },
+  { id: 12, src: photol },
+  { id: 13, src: photom },
+  { id: 14, src: photon },
+  { id: 15, src: photoo },
 ]
 
 export default albumPhoto
